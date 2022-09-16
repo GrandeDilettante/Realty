39f599b684f7691553addb1184397c94ab68e190
Матвей Сергеевич Акулов <s0153343@edu.kubsu.ru>
2022-09-16 20:48:43 +0300

    настроил хибер

--- src/main/java/com/realty/entity/Bid.java
+++ src/main/java/com/realty/entity/Bid.java
@@ -1,23 +1,30 @@
 package com.realty.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.realty.entity.base.BaseEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 @Data
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = true, exclude = "realty")
 @Table(schema = "catalogs", name = "tbid")
 public class Bid extends BaseEntity {
-    private Date createTime;
+
+    @CreationTimestamp
+    @Column(name = "createdate")
+    private Date createDate;
     private Double value;
-    private Long realtyId;
 
 //    @JoinColumn(name = "realty_id", updatable = false)
-//    @NotFound(action = NotFoundAction.IGNORE)
-//    @ManyToOne(targetEntity = Realty.class, fetch = FetchType.EAGER)
+//    @ManyToOne
 //    @JsonIgnore
 //    private Realty realty;
+
+    @Column(name = "realty_id")
+    private Long realtyId;
 }
--- src/main/java/com/realty/entity/Realty.java
+++ src/main/java/com/realty/entity/Realty.java
@@ -4,17 +4,26 @@ import com.realty.entity.base.BaseEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Table(schema = "catalogs", name = "trealty")
 public class Realty extends BaseEntity {
+
     private String name;
     private Long price;
 
-//    @NotFound(action = NotFoundAction.IGNORE)
-//    @OneToMany
-//    @JoinColumn(name = "realty_id")
+//    @OneToMany(mappedBy = "realty")
 //    private Set<Bid> bid;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.MERGE}, orphanRemoval = true)
+    @JoinColumn(name = "realty_id")
+    private Set<Bid> bids;
+
+//    @ElementCollection(fetch = FetchType.EAGER)
+//    @JoinTable(schema = "catalogs", name = "tbid", joinColumns = @JoinColumn(name = "realty_id"))
+//    @Column(name = "id")
+//    private Set<Long> bidIds;
 }
